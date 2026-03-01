4746a7adc29d4817367ee4416acfaac914fed3f9
Aman Singh <amansingh08087@gmail.com>
2026-03-01 23:48:22 +0530

    googlemaps

---
+++ lib/googlemaps/google_maps_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapScreen extends StatefulWidget {
+  const GoogleMapScreen({super.key});
+
+  @override
+  State<GoogleMapScreen> createState() => _GoogleMapScreenState();
+}
+
+class _GoogleMapScreenState extends State<GoogleMapScreen> {
+  GoogleMapController? _controller;
+
+  final LatLng _initialPosition = const LatLng(28.6139, 77.2090);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Google Maps'),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _initialPosition,
+          zoom: 14,
+        ),
+        onMapCreated: (controller) {
+          _controller = controller;
+        },
+        markers: {
+          Marker(
+            markerId: const MarkerId('marker_1'),
+            position: _initialPosition,
+          ),
+        },
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+      ),
+    );
+  }
+}
